dc2e0cea290cab3d4efa71ca388cdc5f2b3ca12f
rusik <rusikss.ss13@gmail.com>
2023-02-01 17:33:26 +0300

    Calculate

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,14 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Calculator {
-    public static void main(String[] args) throws IOException, IOException {
+    public static void main(String[] args) throws IOException, CalculatorException {
         Converter converter = new Converter();
         BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
         String[] str = buff.readLine().split(" ");
         String a = str[0];
         String b = str[1];
         String c = str[2];
-        System.out.println(converter.calculate(a, b, c));
-        System.out.println(converter.romanToInteger(a, b, c));
+        int ch0 = converter.romanToInteger(a);
+        int ch2 = converter.romanToInteger(c);
+        if (ch0 < ch2 && b.equals("-")) {
+            throw new CalculatorException("В римской системе нет отрицательных чисел и нуля");
+        }
+        if (str.length > 3) {
+            throw new CalculatorException("Формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        }
+        if (str[1].equals(null) || c.equals(null)) {
+            throw new CalculatorException("Строка не является математической операцией");
+        }
+        Integer result = converter.calculate(ch0, b, ch2);
+        System.out.println(converter.integerToRoman(result));
+        int f = Integer.parseInt(a);
+        int d = Integer.parseInt(c);
+        if (f <= 10 && d <= 10) {
+            Integer result2 = converter.calculate(f, b, d);
+            System.out.println(result2);
+        } else {
+            throw new CalculatorException("Числа не могут быть более 10");
+        }
     }
 }
+
+
--- src/Converter.java
+++ src/Converter.java
@@ -1,30 +1,18 @@
 public class Converter {
-    public String romanToInteger(String roman, String operator, String roman2) {
-        String[] letters = {" ", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
-        Integer[] result = new Integer[2];
+    public Integer romanToInteger(String roman) {
+        String letters[] = {" ", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
         for (int i = 0; i < letters.length; i++) {
             if (letters[i].equals(roman)) {
-                result[0] = i;
+                return i;
             }
         }
-        for (int j = 0; j < letters.length; j++) {
-            if (letters[j].equals(roman2)) {
-                result[1] = j;
-            }
-
-        }
-        Integer x = calculate2(result[0], operator, result[1]);
-        return integerToRoman(x);
-
-
+        return 0;
     }
-
-
 
     public String integerToRoman(Integer number) {
         StringBuffer result = new StringBuffer();
-        int[] uniqueNumber = new int[]{1, 4, 5, 9, 10, 40, 50};
-        String[] arrSymbol = new String[]{"I", "IV", "V", "IX", "X", "XL", "L", "C"};
+        int[] uniqueNumber = new int[] {1, 4, 5, 9, 10, 40, 50};
+        String[] arrSymbol = new String[] {"I", "IV", "V", "IX", "X", "XL", "L", "C"};
         int size = uniqueNumber.length - 1;
         while (number > 0) {
             int count = number / uniqueNumber[size];
@@ -38,7 +26,7 @@ public class Converter {
         return result.toString();
     }
 
-    public int calculate2(Integer a, String operator, Integer b) {
+    public Integer calculate(Integer a, String operator, Integer b) {
         int sum = 0;
         switch (operator) {
             case "+":
@@ -52,30 +40,6 @@ public class Converter {
                 break;
             case "/":
                 sum = a / b;
-                break;
-
-        }
-        return sum;
-
-    }
-
-
-    public Integer calculate(String a, String operator, String b) {
-        int f = Integer.parseInt(a);
-        int d = Integer.parseInt(b);
-        int sum = 0;
-        switch (operator) {
-            case "+":
-                sum = f + d;
-                break;
-            case "-":
-                sum = f - d;
-                break;
-            case "*":
-                sum = f * d;
-                break;
-            case "/":
-                sum = f / d;
                 break;
 
         }
